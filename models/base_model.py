893d6d10267ea5d166cb081270d8c88230645a63
mahi7000 <mahlet7000@gmail.com>
2024-04-14 02:28:15 -0700

    Add kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -6,10 +6,20 @@ import uuid
 
 class BaseModel:
     """ class containing common attributes """
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            kwargs.pop('__class__', None)
+
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value,
+                            "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
