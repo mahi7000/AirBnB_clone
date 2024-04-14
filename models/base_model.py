70685b90ba81fe204be791029432e73b196c9362
mahi7000 <mahlet7000@gmail.com>
2024-04-14 02:14:41 -0700

    base models

--- models/base_model.py
+++ models/base_model.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
-""" defines all common attributes """
-import uuid
+""" contains a class BaseModel that defines all common """
 from datetime import datetime
+import uuid
 
 
 class BaseModel:
-    """ defines common attributes/methods for other classes """
+    """ class containing common attributes """
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
-        """ updates update_at with current datetime """
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """ returns dict containing all values of __dict__ """
-        data = self.__dict__.copy()
-        data['__class__'] = self.__class__.__name__
-        data['created_at'] = self.created_at.isoformat()
-        data['updated_at'] = self.updated_at.isoformat()
-        return data
-
-    def __str__(self):
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
