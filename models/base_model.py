0bc9cdd19e5b147941e00c4bb1e88bfdb1e7ada0
Your Name <mahlet7000>
2024-07-01 00:10:45 -0700

    kwargs added

--- models/base_model.py
+++ models/base_model.py
@@ -9,11 +9,21 @@ from datetime import datetime
 class BaseModel:
     """Contains common attributes/methods"""
 
-    def __init__(self):
+    def __init__(self,*args, **kwargs):
         """Creates public instance attributes"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                setattr(self, key, value)
+            if 'created_at' in kwargs:
+                self.created_at = datetime.strptime(kwargs['created_at'], '%Y-%m-%d %H:%M:%S')
+            if 'updated_at' in kwargs:
+                self.updated_at = datetime.strptime(kwargs['updated_at'], '%Y-%m-%d %H:%M:%S')
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """Prints string representaion of object"""
