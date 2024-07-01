97bc20304a6b1cbe051ebf8f6b20b0e8a1190406
Your Name <mahlet7000>
2024-06-30 22:59:08 -0700

    Add basemodel and tests

--- models/base_model.py
+++ models/base_model.py
@@ -1,35 +1,31 @@
 #!/usr/bin/python3
-""" contains a class BaseModel that defines all common """
-from models import storage
-from datetime import datetime
+"""Contains class BaseModel that defines
+    all common attributes/methods for other classes"""
+
 import uuid
+from datetime import datetime
 
 
 class BaseModel:
-    """ class containing common attributes """
-    def __init__(self, *args, **kwargs):
-        if kwargs:
-            kwargs.pop('__class__', None)
+    """Contains common attributes/methods"""
 
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(value,
-                            "%Y-%m-%dT%H:%M:%S.%f"))
-                else:
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
+    def __init__(self):
+        """Creates public instance attributes"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
-        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+        """Prints string representaion of object"""
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
+        """Updates 'updated_at' with current datetime"""
         self.updated_at = datetime.now()
-        storage.save()
 
     def to_dict(self):
+        """Returns a dictionary with values of __dict__"""
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
---
+++ tests/test_base_model.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Unittest for class BaseModel"""
+
+from models.base_model import BaseModel
+import unittest
+from datetime import datetime, timedelta
+
+
+class TestBaseModel(unittest.TestCase):
+    def setUp(self):
+        self.model = BaseModel()
+
+    def test_attributes(self):
+        self.assertTrue(hasattr(self.model, 'id'))
+        self.assertTrue(hasattr(self.model, 'created_at'))
+        self.assertTrue(hasattr(self.model, 'updated_at'))
+
+    def test_id_generation(self):
+        self.assertIsInstance(self.model.id, str)
+
+    def test_created_at(self):
+        self.assertIsInstance(self.model.created_at, datetime)
+
+    def test_updated_at_initial(self):
+        self.assertIsInstance(self.model.updated_at, datetime)
+        self.assertEqual(self.model.created_at, self.model.updated_at)
+
+    def test_updated_at_save(self):
+        initial = self.model.updated_at
+        self.model.save()
+        self.assertNotEqual(initial, self.model.updated_at)
+
+    def test_to_dict(self):
+        obj_dict = self.model.to_dict()
+        self.assertIsInstance(obj_dict, dict)
+        self.assertEqual(obj_dict['__class__'], 'BaseModel')
+        self.assertEqual(obj_dict['id'], self.model.id)
+
+        created_at = datetime.strptime(
+                obj_dict['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+        updated_at = datetime.strptime(
+                obj_dict['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+
+        self.assertEqual(created_at, self.model.created_at)
+        self.assertEqual(updated_at, self.model.updated_at)
+
+
+if __name__ == '__main__':
+    unittest.main()
