0ddbd27b4a07ebe1230bde036757e965d218cce5
Your Name <mahlet7000>
2024-07-05 00:26:56 -0700

    Updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,18 +25,16 @@ class FileStorage:
             for key,value in self.__objects.items():
                 json.dump({key: value.to_dict()}, file)
 
-    def reload(self):
-        """Deserialize JSON to __objects"""
-        try:
-            with open(self.__file_path, 'r') as file:
-                for line in file:
-                    data = json.load(file)
-                    from models.base_model import BaseModel
-                    classes = {'BaseModel': BaseModel}
-                    for key, value in data.items():
-                        cls, obj_id = key.split('.')
-                        obj = classes[cls](**value)
-                        self.__objects[key] = obj
+        def reload(self):
+        """Deserialize the JSON file __file_path to __objects, if it exists."""
+        if (os.path.isfile(self.__file_path)
+                and os.path.getsize(self.__file_path) > 0):
+            with open(self.__file_path, 'r') as f:
+                self.__objects = {k: self.get_class(k.split(".")[0])(**v)
+                                  for k, v in json.load(f).items()}
 
-        except FileNotFoundError:
-            pass
+    def get_class(self, name):
+        """ returns a class from models module using its name"""
+        sub_module = re.sub('(?!^)([A-Z]+)', r'_\1', name).lower()
+        module = importlib.import_module(f"models.{sub_module}")
+        return getattr(module, name)
