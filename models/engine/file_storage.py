cc177dd4f945fedf59cd7a7ab67ba4aae6759e60
Your Name <mahlet7000>
2024-07-04 23:36:29 -0700

    updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,43 +1,44 @@
 #!/usr/bin/python3
-"""Serializes instances to JSON file and
-Deserializes JSON file to instances"""
+"""Serializes instances to JSON file and back"""
 
 import json
 
 
 class FileStorage:
-    """Serialize and deserialize"""
+    """Serializes and deserializes instances and JSON"""
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """Return dictionary objects"""
+        """Returns dictionary __objects"""
         return self.__objects
 
     def new(self, obj):
-        """Sets obj with key <obj classname.id>"""
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__object[key] = obj
+        """sets in __objects the obj"""
+        cls = obj.__class__.__name__
+        key = "{}.{}".format(cls, str(obj.id))
+        self.__objects[key] = obj
 
     def save(self):
-        """Serializes objects to JSON file"""
+        """serialize __objects to JSON"""
         serialized = {}
-        for key, obj in self.__objects.items():
-            serialized[key] = obj.to_dict()
-
-        with open(self.__file_path, "w") as file:                       json.dump(serialized, file)
+        for key, value in self.__objects.items():
+            serialized[key] = value.to_dict()
+        with open(self.__file_path, 'w') as file:
+            for key in serialized.keys():
+                json.dump(serialized, file)
+                file.write("\n")
 
     def reload(self):
-        """Deserializes JSON file to objects"""
         try:
-            with open(self.__file_path, "r") as file:
-                serialized = json.load(file)
+            with open(self.__file_path, 'r') as file:
+                for line in f:
+                    data = json.load(line)
 
-                for key, value in serialized.items():
-                    class_name, obj_id = key.split('.')
-                    cls = eval(class_name)
-                    obj = cls(**value)
-                    self.__objects[key] = obj
+                    for key, value in data.items():
+                        cls, obj_id = key.split('.')
+                        obj = eval(cls)(**value)
+                        self.__objects[key] = obj
 
         except FileNotFoundError:
             pass
