9f3ad76994040aa23452b183ff96ba8c76dd5e8f
Your Name <mahlet7000>
2024-07-05 00:42:24 -0700

    Updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -22,19 +22,21 @@ class FileStorage:
     def save(self):
         """serialize __objects to JSON"""
         with open(self.__file_path, 'w') as file:
-            for key,value in self.__objects.items():
+            for key, value in self.__objects.items():
                 json.dump({key: value.to_dict()}, file)
 
-        def reload(self):
-        """Deserialize the JSON file __file_path to __objects, if it exists."""
-        if (os.path.isfile(self.__file_path)
-                and os.path.getsize(self.__file_path) > 0):
-            with open(self.__file_path, 'r') as f:
-                self.__objects = {k: self.get_class(k.split(".")[0])(**v)
-                                  for k, v in json.load(f).items()}
+    def reload(self):
+        """Deserialize the JSON file to instances """
+        try:
+            from models.base_model import BaseModel
+            classes = {'BaseModel', BaseModel}
 
-    def get_class(self, name):
-        """ returns a class from models module using its name"""
-        sub_module = re.sub('(?!^)([A-Z]+)', r'_\1', name).lower()
-        module = importlib.import_module(f"models.{sub_module}")
-        return getattr(module, name)
+            with open(self.__file_path, 'r') as file:
+                content = file.read()
+                if content:
+                    serialized = json.loads(content)
+                    for key, value in serialized.items():
+                        cls, objid = key.split('.')
+                        self.__objects[key] = classes[cls](**value)
+        except FileNotFoundError:
+            pass
